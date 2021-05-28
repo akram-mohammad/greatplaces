e128c10354e4c4ffda08cd840270f86cf3fec097
Akram Mohamed <akram.mohamed0@hotmail.com>
2021-05-28 14:05:52 +0200

    Place Model Added

--- lib/models/place.dart
+++ lib/models/place.dart
@@ -1,1 +1,30 @@
-class Place {}
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+
+class Place {
+  final int id;
+  final String title;
+  final String desciption;
+  final PlaceLocation location;
+  final File image;
+
+  Place(
+      {@required this.id,
+      @required this.title,
+      this.desciption,
+      @required this.location,
+      @required this.image});
+}
+
+class PlaceLocation {
+  final double longitude;
+  final double latitude;
+  final String address;
+
+  PlaceLocation({
+    @required this.longitude,
+    @required this.latitude,
+    this.address,
+  });
+}
